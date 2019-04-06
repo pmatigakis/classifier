463f4546991b0ccfd599e5073922a987d69f6891
Panagiotis Matigakis <pmatigakis@gmail.com>
2019-04-06 19:56:57 +0300

    Updated the docker file so that it can build the latest version

--- Dockerfile
+++ Dockerfile
@@ -6,8 +6,8 @@ ADD . /app
 
 RUN pip install scipy==0.18.0 numpy==1.11.1
 RUN pip install scikit-learn==0.17.1
-RUN pip install git+https://github.com/topicaxis/trained-models.git
 RUN python setup.py install
+RUN pip install git+https://github.com/topicaxis/trained-models.git@0.4.0
 RUN cp configuration/settings.py .
 
 EXPOSE 8022
--- configuration/settings.py
+++ configuration/settings.py
@@ -1,11 +1,14 @@
+from distutils.util import strtobool
 import os
 import re
 
-from classifiers.classifiers import CategoryClassifier
+from classifiers.documents.categories.classifier import CategoryClassifier
+from classifiers.keyword_selection.classifier import KeywordSelectionClassifier
 
 from classifier.ml import Classifier
 
-DEBUG = False
+DEBUG = strtobool(os.getenv("DEBUG", "False"))
+TESTING = strtobool(os.getenv("TESTING", "False"))
 
 PROPAGATE_EXCEPTIONS = True
 
@@ -13,7 +16,8 @@ CLASSIFIERS = {
     "categories": Classifier(
         classifier=CategoryClassifier(),
         probabilities=True
-    )
+    ),
+    "select-keywords": Classifier(classifier=KeywordSelectionClassifier())
 }
 
 
@@ -30,18 +34,34 @@ if host is None:
     raise RuntimeError("failed to find the container ip address")
 
 HOST = host
-PORT = os.getenv("PORT", 8022)
+PORT = int(os.getenv("PORT", 8022))
 
 WORKER_MAX_REQUESTS = int(os.getenv("WORKER_MAX_REQUESTS", 500))
 WORKER_MAX_REQUESTS_JITTER = int(os.getenv("WORKER_MAX_REQUESTS_JITTER", 30))
 WORKERS = int(os.getenv("WORKERS", 4))
 
 CONSUL_HOST = os.getenv("CONSUL_HOST")
-CONSUL_PORT = os.getenv("CONSUL_PORT", 8500)
-CONSUL_SCHEME = "http"
-CONSUL_VERIFY_SSL = True
+CONSUL_PORT = int(os.getenv("CONSUL_PORT", 8500))
+CONSUL_SCHEME = os.getenv("CONSUL_SCHEME", "http")
+CONSUL_VERIFY_SSL = strtobool(os.getenv("CONSUL_VERIFY_SSL", "True"))
 CONSUL_HEALTH_INTERVAL = "10s"
 CONSUL_HEALTH_TIMEOUT = "5s"
+
+__handlers = {
+    'console': {
+        'level': os.getenv("CONSOLE_LOG_LEVEL", "INFO"),
+        'class': 'logging.StreamHandler',
+        'formatter': 'verbose'
+    }
+}
+
+__sentry_dsn = os.getenv("SENTRY_DSN")
+if __sentry_dsn:
+    __handlers["sentry"] = {
+        'level': os.getenv("SENTRY_LOG_LEVEL", "ERROR"),
+        'class': 'raven.handlers.logging.SentryHandler',
+        'dsn': __sentry_dsn
+    }
 
 LOGGING = {
     'version': 1,
@@ -51,24 +71,18 @@ LOGGING = {
             'format': "%(asctime)s %(levelname)s [%(process)d:%(thread)d] %(name)s [%(pathname)s:%(funcName)s:%(lineno)d] %(message)s"
         }
     },
-    'handlers': {
-        'console': {
-            'level': 'INFO',
-            'class': 'logging.StreamHandler',
-            'formatter': 'verbose'
-        }
-    },
+    'handlers': __handlers,
     'loggers': {
         'classifier': {
-            'handlers': ['console'],
+            'handlers': __handlers.keys(),
             'propagate': True
         },
         'classifiers': {
-            'handlers': ['console'],
+            'handlers': __handlers.keys(),
             'propagate': True
         }
     },
     "root": {
-        'level': 'INFO',
+        'level': os.getenv("ROOT_LOGGER_LEVEL", "INFO"),
     }
 }
