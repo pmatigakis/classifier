8d3487dbfd2bc6efab6fd98a9dc82c095756cd36
Panagiotis Matigakis <pmatigakis@gmail.com>
2019-10-26 14:39:26 +0300

    Bumped the requirements to their latest versions

--- setup.py
+++ setup.py
@@ -1,32 +1,36 @@
 from setuptools import setup, find_packages
 
 
+def read_requirements_file(requirements_file):
+    with open(requirements_file) as f:
+        requirements = [
+            line.strip()
+            for line in f
+        ]
+
+    return requirements
+
+
 setup(
     name="classifier",
     version="0.7.0",
     packages=find_packages(),
-    install_requires=[
-        "scikit-learn==0.17.1",
-        "scipy==0.18.0",
-        "numpy==1.11.1",
-        "Flask==0.11.1",
-        "Flask-RESTful==0.3.5",
-        "requests==2.11.1",
-        "beautifulsoup4==4.5.1",
-        "Flask-Script==2.0.5",
-        "gunicorn==19.7.1",
-        "python-consul==0.7.0",
-        "raven==6.1.0",
-        "python-dotenv==0.10.3"
-    ],
+    install_requires=read_requirements_file("requirements.txt"),
     test_suite='nose.collector',
-    tests_require=[
-        "nose==1.3.7",
-        "mock==2.0.0"
-    ],
+    tests_require=read_requirements_file("requirements-test.txt"),
     include_package_data=True,
     zip_safe=False,
     entry_points={
         'console_scripts': ['classifier-cli=classifier.cli:main'],
-    }
+    },
+    classifiers=(
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Natural Language :: English',
+        "Programming Language :: Python :: 3",
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: Implementation :: CPython',
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    )
 )
